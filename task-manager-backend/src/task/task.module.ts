263850aafd704109505657f548eb8feaa0725893
Santiago <santiago.ottolini98@gmail.com>
2025-06-02 19:38:34 +0200

    Fixing errors

--- task-manager-backend/src/task/task.module.ts
+++ task-manager-backend/src/task/task.module.ts
@@ -3,8 +3,12 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 
 // Módulo de tareas: agrupa el servicio y el controlador
+import { AuthModule } from '../auth/auth.module';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
 @Module({
-  providers: [TaskService],
+  imports: [AuthModule], // Importa AuthModule para acceso a AuthService
+  providers: [TaskService, JwtAuthGuard],
   controllers: [TaskController],
 })
 export class TaskModule {}
--- task-manager-backend/src/user/user.module.ts
+++ task-manager-backend/src/user/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { AuthModule } from '../auth/auth.module';
 
 // Módulo de usuario: agrupa el servicio y el controlador
 @Module({
+  imports: [AuthModule], // Importa AuthModule para acceso a AuthService
+  providers: [UserService, JwtAuthGuard],
   controllers: [UserController],
-  providers: [UserService],
 })
 export class UserModule {}
